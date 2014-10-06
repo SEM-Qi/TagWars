dd20ac109630f79e5cbc02c18319d989fa9c8d12
Henrik <henrik.persson1992@hotmail.com>
2014-10-06 12:51:21 +0200

    name logic added

---
+++ frontend/Assets/scripts/NameLogic.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var pName:GameObject;
+var oName:GameObject;
+
+var playerName:String;
+var opponentName:String;
+
+function Start () {
+pName.GetComponentsInChildren.<UI.Text>()[0].text = playerName;
+oName.GetComponentsInChildren.<UI.Text>()[0].text = opponentName;
+
+}
