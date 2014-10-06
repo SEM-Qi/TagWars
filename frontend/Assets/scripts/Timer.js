f6b174c73727c4936a8c45d437b1f30ba1ee5906
Henrik <henrik.persson1992@hotmail.com>
2014-10-06 14:42:06 +0200

    timer added

---
+++ frontend/Assets/scripts/Timer.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var timer:float = 5.0;
+var tDisplay:GameObject;
+var tString:String;
+
+function Update () {
+ if(timer > 0.5){
+  timer -= Time.deltaTime;
+  tString = timer.ToString("0");
+  tDisplay.GetComponentsInChildren.<UI.Text>()[0].text = tString;
+ }
+ if(timer <= 0.5){
+  timer = 0;
+  tString = '#FIGHT';
+  tDisplay.GetComponentsInChildren.<UI.Text>()[0].text = tString;
+ }
+}
