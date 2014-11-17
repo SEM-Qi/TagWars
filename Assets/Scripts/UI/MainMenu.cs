a00c387c809cb682deacd8a9bf26e40a3703f1ff
AureFranky <au.re@live.com>
2014-11-17 14:56:58 +0100

    adding gitignore

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,11 +8,11 @@ public class MainMenu : MonoBehaviour {
     public Button startButton;
     public Button exitButton;
 
-    public Controller controller;
+    // public Controller controller;
 
 	void Start () {
-        controller = GetComponent<Controller>();
-        startButton.onClick.AddListener(() => { Debug.Log("START"); Application.LoadLevel("Battle"); controller.StartMultiplayer(); });
+        // controller = GetComponent<Controller>();
+        startButton.onClick.AddListener(() => { Debug.Log("START"); Application.LoadLevel("Battle"); /*controller.StartMultiplayer();*/ });
         exitButton.onClick.AddListener(() => { Debug.Log("EXIT"); Application.Quit(); });
 	}
 }
